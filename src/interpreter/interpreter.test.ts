4b5deba89f297bf9da1bfa8f439d4547f544d226
Thomas Brq <71637888+thomasbrq@users.noreply.github.com>
2024-02-27 14:42:49 +0100

    fix: added tests

--- src/interpreter/interpreter.test.ts
+++ src/interpreter/interpreter.test.ts
@@ -66,4 +66,112 @@ describe("Interpreter", () => {
     expect(result.value).toBe("null");
     expect(result.type).toBe("null");
   });
+
+  test("declare variable with a value", () => {
+    const lexer = new Lexer("var a = 1;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    const v = env.get("a");
+    expect(v.value).toBe(1);
+  });
+
+  test("declare variable without value", () => {
+    const lexer = new Lexer("var a;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    const v = env.get("a");
+    expect(v.value).toBe("null");
+  });
+
+  test("declare const with a value", () => {
+    const lexer = new Lexer("const abc = 10;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    const v = env.get("abc");
+    expect(v.value).toBe(10);
+  });
+
+  test("assign a new value to an initialized variable", () => {
+    const lexer = new Lexer("var hello = 0;\nhello = 15;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    const v = env.get("hello");
+    expect(v.value).toBe(15);
+  });
+
+  test("assign a new value to a declared variable but not initialized", () => {
+    const lexer = new Lexer("var hello;\nhello = 777;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    const v = env.get("hello");
+    expect(v.value).toBe(777);
+  });
+
+  test("assign a variable with another variable", () => {
+    const lexer = new Lexer("var one = 1;\nvar two = one + 1;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    expect(env.get("one").value).toBe(1);
+    expect(env.get("two").value).toBe(2);
+  });
+
+  test("assign a variable with a const", () => {
+    const lexer = new Lexer("const hate_u = 0;\nvar love_u = hate_u + 1;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    expect(env.get("hate_u").value).toBe(0);
+    expect(env.get("love_u").value).toBe(1);
+  });
+
+  test("init a const with a var", () => {
+    const lexer = new Lexer("var likes = 10;\nconst n_likes = likes;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    expect(env.get("likes").value).toBe(10);
+    expect(env.get("n_likes").value).toBe(10);
+  });
+
+  test("init a const with a const", () => {
+    const lexer = new Lexer("const teamo = 1000;\nconst yotambien = teamo;");
+    const parser = new Parser(lexer);
+    const program = parser.parse();
+
+    const env = new Environment();
+    const result = evaluate(program, env);
+
+    expect(env.get("teamo").value).toBe(1000);
+    expect(env.get("yotambien").value).toBe(1000);
+  });
 });
