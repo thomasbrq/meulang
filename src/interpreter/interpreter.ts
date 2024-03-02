561f0bbf1512832eebb8add8d1936504c6ab6167
Thomas Brq <71637888+thomasbrq@users.noreply.github.com>
2024-03-02 15:24:05 +0100

    feat: handle test operators

--- src/interpreter/interpreter.ts
+++ src/interpreter/interpreter.ts
@@ -70,6 +70,31 @@ function evaluate_binary_operations(
         result = lhs.value / rhs.value;
       }
       break;
+    case "==":
+      {
+        result = lhs.value == rhs.value;
+      }
+      break;
+    case ">":
+      {
+        result = lhs.value > rhs.value;
+      }
+      break;
+    case ">=":
+      {
+        result = lhs.value >= rhs.value;
+      }
+      break;
+    case "<":
+      {
+        result = lhs.value < rhs.value;
+      }
+      break;
+    case "<=":
+      {
+        result = lhs.value <= rhs.value;
+      }
+      break;
   }
 
   return {
