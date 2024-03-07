f93a719e0e0df14fff188867f3acc6a5a79d151b
Thomas Brq <71637888+thomasbrq@users.noreply.github.com>
2024-03-07 22:02:09 +0100

    Parse the dot of the float number.

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -48,7 +48,7 @@ export class Lexer {
     if (character.length > 1) {
       return false;
     }
-    return character >= "0" && character <= "9";
+    return (character >= "0" && character <= "9") || character == ".";
   }
 
   protected is_alpha(character: string): boolean {
